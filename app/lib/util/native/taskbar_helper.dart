4e71445750598243a6549eb62dcbe57ed447aed3
sun <sun@manjaro.lan>
2026-07-06 23:57:41 +0800

    fix

--- app/lib/util/native/taskbar_helper.dart
+++ app/lib/util/native/taskbar_helper.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:localsend_app/util/native/platform_check.dart';
-import 'package:macos_dock_progress/macos_dock_progress.dart';
 import 'package:windows_taskbar/windows_taskbar.dart';
 
 class TaskbarHelper {
   static final _isWindows = checkPlatform([TargetPlatform.windows]);
-  static final _isMacos = checkPlatform([TargetPlatform.macOS]);
 
   static Future<void> clearProgressBar() async {
     if (_isWindows) {
       await WindowsTaskbar.setProgressMode(TaskbarProgressMode.noProgress);
-    } else if (_isMacos) {
-      await DockProgress.setProgress(1.0);
     }
   }
 
@@ -19,8 +15,6 @@ class TaskbarHelper {
     if (total != double.minPositive.toInt() && total != double.maxFinite.toInt()) {
       if (_isWindows) {
         await WindowsTaskbar.setProgress(progress, total);
-      } else if (_isMacos) {
-        await DockProgress.setProgress(double.parse((progress / total).toStringAsFixed(3)));
       }
     } else {
       if (_isWindows) {
